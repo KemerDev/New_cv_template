64fb4ec0cffe8cd2fe88045f22f1e142599fe69b
kemerios <kemerios10@gmail.com>
2022-02-10 19:42:54 +0200

    see

--- assets/index.js
+++ assets/index.js
@@ -92,8 +92,8 @@ $.getJSON("assets/info.json", function(data) {
 
     $(document).ready(function() {
         for(x in data.Person.Skills) {
-            $('.skills__content').append("<ul class='skills__data'></ul>");
             for (y in data.Person.Skills[x]) {
+                $('.skills__content').append("<ul class='skills__data'></ul>");
                 $('.skills__data').append("<li class='skills__name'><span class='skills__circle'>"+y+"</span></li>");
             }
         }
