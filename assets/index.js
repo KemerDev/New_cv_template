d24ab953f4f558bd2ca5052e2a3cb2c7a7214ea3
kemerios <kemerios10@gmail.com>
2022-02-12 16:53:43 +0200

    see

--- assets/index.js
+++ assets/index.js
@@ -67,7 +67,7 @@ let opt = {
     filename: 'GeorgiosCV.pdf',
     image: {type: 'jpeg', quality: 0.98},
     html2canvas: {scale: 4},
-    jsPDF: {format: 'a3', orientation: 'portrait'}
+    jsPDF: {format: 'a4', orientation: 'portrait'}
 }
 
 
