bd64a54d642346897dd8e0bd891477d0de74e084
kemerios <kemerios10@gmail.com>
2022-05-01 21:29:32 +0200

    test

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ resumeButton.addEventListener('click', () => {
 })
 
 /* -----------------------------Some Json Stuff------------------------------- */
-$.getJSON("assets/info.json", function(data) {
+$.getJSON("info.json", function(data) {
     document.getElementById("title").innerHTML = data.Person.Name + " " + data.Person.Surname;
     document.getElementById("profession").innerHTML = data.Person.Info;
     document.getElementById("description").innerHTML = data.Person.About;
